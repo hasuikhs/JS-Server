a1cee2d4b250bbcb06000877cc0404a5e7eb4325
Hanseok <hasuikhs@gmail.com>
2021-11-23 22:44:39 +0900

    211123|Update express server

--- ExpressJS/src/index.ts
+++ ExpressJS/src/index.ts
@@ -1,13 +1,44 @@
-import express, { Router } from 'express';
+import express, { Request, Response, Router } from 'express';
+import cluster from 'cluster'
+import os from 'os';
 import cors from 'cors';
 
-const runServer = (() => {
-    const app = express(), port = 4000;
+const PORT = process.env.PORT || 4000;
+const ClusterWorkerSize = os.cpus().length;
+
+if (ClusterWorkerSize > 1) {
+    if (cluster.isMaster) { // v16+ deprecated
+        for (let i = 0; i < ClusterWorkerSize; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker) => {
+            console.log('Worker', worker.id, ' has exitted.');
+        })
+    } else {
+        runServer();
+    }
+} else {
+    runServer(true);
+}
+
+function runServer(isSingle = false) {
+    const app = express();
 
     app.use(express.urlencoded({extended: true})).use(cors());
 
     app.get('/', (req, res) => res.json({message: 'Hello World'}));
 
-    app.listen(port, () => console.log(`http://localhost:${port} started...`));
+    app.get('/test/:second', (req: Request<any>, res: Response) => {
+        console.time(req.params.second)
+        console.log('test')
+        setTimeout(() => {
+            console.timeEnd(req.params.second)
+            res.json({info: `delay ${req.params.second} second...`});
+        }, req.params.second * 1_000);
+    });
 
-})();
+    app.listen(PORT, () => {
+        console.log(`Express server listening on port ${PORT} ${isSingle ? 'with the single worker' : 'and worker'} ${process.pid}.`)
+    });
+};
