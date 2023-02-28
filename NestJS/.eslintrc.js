40b1f74a42713cb6faf6238e0166322867b74bc6
hanseok kim <hasuikhs@gmail.com>
2023-02-28 23:01:27 +0900

    230228|Update nest

--- NestJS/.eslintrc.js
+++ NestJS/.eslintrc.js
@@ -1,9 +1,8 @@
-// 개발자들이 특정한 규칙을 가지고 코드를 깔끔하게 짤수있게 도와주는 라이브러리
-// 타입스크립트를 쓰는 가이드 라인 제시, 문법에 오류가 나면 알려주는 역할 등
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
