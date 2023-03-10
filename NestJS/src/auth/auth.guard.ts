4255d9afd54abdf8fcb992aef131b675e8081a67
HanSeok Kim <hasuikhs@gmail.com>
2023-03-10 17:13:13 +0900

    230310|Modify

--- NestJS/src/auth/auth.guard.ts
+++ NestJS/src/auth/auth.guard.ts
@@ -7,7 +7,7 @@ export class AuthGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log('this is guard');
+
     return true;
   }
 }
