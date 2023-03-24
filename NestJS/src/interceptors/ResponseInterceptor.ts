f2bb1070c7392480004c1af0f48b6ed57f0338f6
HanSeok Kim <hasuikhs@gmail.com>
2023-03-24 10:42:21 +0900

    230324|Update responseInterceptor handler

--- NestJS/src/interceptors/ResponseInterceptor.ts
+++ NestJS/src/interceptors/ResponseInterceptor.ts
@@ -5,17 +5,57 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-    return next
+    const headers = [{
+      key: 'foo',
+      value: 'bar'
+    }];
+    const cookies = [{
+      key: 'test',
+      value: 'hi'
+    }];
+    
+    return responseHandler(context, next, headers, cookies, { httpOnly: true });
+  }
+}
+
+interface Header {
+  key: string;
+  value: any;
+}
+
+interface Cookie {
+  key: string;
+  value: any;
+}
+
+interface CookieOption {
+  httpOnly?: boolean;
+  secure?: boolean;
+}
+
+const responseHandler = (context: ExecutionContext, next: CallHandler<any>, headers: Header[] = [], cookies: Cookie[] = [], option: CookieOption = {}): Observable<any> => {
+  return (
+    next
       .handle()
       .pipe(
         tap(() => {
           const res = context.switchToHttp().getResponse();
 
-          res.header('foo', 'bar');
-          res.cookie('test', 'hi', {
-            httpOnly: true
-          });
+          if (headers.length > 0) {
+            headers.forEach(item => {
+              res.header(item.key, item.value);
+            });
+          }
+
+          if (cookies.length > 0) {
+            cookies.forEach(item => {
+              res.cookie(item.key, item.value, {
+                httpOnly: option.httpOnly || false,
+                secure: option.secure || false
+              });
+            });
+          }
         })
-      );
-  }
+      )
+  );
 }
